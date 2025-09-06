2accf0d166e8d707f7d0804480659359750fee54
drawfig <usmckdm@gmail.com>
2025-09-05 22:29:38 -0700

    getting readdy for genral use

--- src/routes/Request_Routes.php
+++ src/routes/Request_Routes.php
@@ -1,8 +1,5 @@
 <?php
 
 class Request_Routes {
-    public $REQUEST_ROUTES = [
-        "test" => ["class" => "test_handler", "method" => "test_method", "protected" => false],
-        "secure_test" => ["class" => "test_handler", "method" => "test_secure_method", "protected" => true]
-    ];
+    public $REQUEST_ROUTES = [];
 }
