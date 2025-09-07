e1b83c2aea6f0713776a6224c4eba0663a79b5ee
drawfig <usmckdm@gmail.com>
2025-09-07 11:31:27 -0700

    added a default handler and a test method to keep the 'Handlers' directory open in the repo as well as give an example to users how the structure works in the framework

---
+++ src/Handlers/default_handler.php
@@ -0,0 +1,32 @@
+<?php
+
+spl_autoload_register(function ($class_name) {
+    if(file_exists(__DIR__ . "/Utils/" . str_replace("Utils\\", "", $class_name) . ".php")) {
+        require_once (__DIR__ . "/Utils/" . str_replace("Utils\\", "", $class_name) . ".php");
+    }
+});
+
+spl_autoload_register(function ($class_name) {
+    include ($class_name . ".php");
+});
+
+class default_handler {
+    private $SECRET;
+    private $DATA;
+    private $FD;
+    private $SERVER;
+    private $DB;
+
+    public function __construct($secret, $data, $fd, $server, $db) {
+        $this->SECRET = $secret;
+        $this->DATA = $data;
+        $this->FD = $fd;
+        $this->SERVER = $server;
+        $this->DB = $db;
+    }
+
+    public function bounce() {
+        print("Boing!");
+        $this->SERVER->push($this->FD, json_encode($this->DATA));
+    }
+}
--- src/routes/Request_Routes.php
+++ src/routes/Request_Routes.php
@@ -1,5 +1,7 @@
 <?php
 
 class Request_Routes {
-    public $REQUEST_ROUTES = [];
+    public $REQUEST_ROUTES = [
+        "bounce" => ["class" => "default_handler", "method" => "bounce", "protected" => false],
+    ];
 }
