f7e0bfc81ad3214bd339e50a40672f3f81052412
drawfig <usmckdm@gmail.com>
2025-09-22 20:25:55 -0700

    tweaks to the agent

--- src/Agents/default_agent.php
+++ src/Agents/default_agent.php
@@ -1,6 +1,15 @@
 <?php
 
 namespace Agents;
+spl_autoload_register(function ($class_name) {
+    if(file_exists(__DIR__ . "/Utils/" . str_replace("Utils\\", "", $class_name) . ".php")) {
+        require_once (__DIR__ . "/Utils/" . str_replace("Utils\\", "", $class_name) . ".php");
+    }
+});
+
+spl_autoload_register(function ($class_name) {
+    include ($class_name . ".php");
+});
 
 class default_agent {
     public function bounce_txt() {
